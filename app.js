d4b9e288d0cc57d3e7e0e76b07ee950cf6b5085a
Ira Hudgin <irahudgin@gmail.com>
2022-09-26 18:52:29 -0400

    in process of adding OAuth2

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const path = require("path");
 const express = require("express");
 const db = require("./utils/dbcreate");
 const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // initialize app
 app.use(express.json());
+// set static folder
 app.use(express.static(__dirname + "/views"));
-const port = process.env.PORT || 5000;
-
+// pug setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Home
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/auth", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
 });
 
 // Start server
