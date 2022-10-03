e940d6e1a101ad86adafd2a5bf1cca699ecba70f
Ira Hudgin <irahudgin@gmail.com>
2022-10-03 17:14:11 -0400

    added apology page to prevent issues with registration

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require("express");
 const db = require("./utils/dbcreate");
 const http = require("http");
 const app = express();
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const auctionHelp = require("./utils/helpers");
 
+// for parsing body of form request
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/views"));
@@ -35,8 +38,34 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  // req.headers["Content-Type"] = "application/json";
-  console.log(JSON.stringify(req.body));
+  console.log(req.body.username);
+  let saltRounds = 10;
+  let username = req.body.username;
+  let password = req.body.password;
+  let confirmation = req.body.confirmation;
+
+  if (username === "") {
+    return res.render("apology", {
+      message: "Must include username",
+      code: 400,
+    });
+  } else if (password === "") {
+    return res.render("apology", {
+      message: "Must include password",
+      code: 400,
+    });
+  } else if (!(password === confirmation)) {
+    return res.render("apology", {
+      message: "Passwords must match",
+      code: 400,
+    });
+  }
+  // else if (password == "") {
+  //   return apology("Must enter password");
+  // } else if (!(password === confirmation)) {
+  //   return apology("Passwords do not match");
+  // }
+  res.redirect("/");
 });
 
 // Start server
